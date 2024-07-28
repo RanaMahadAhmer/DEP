7ec669a180324ee8839e32b9070bd1c46a8f8572
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-28 20:20:57 +0500

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.androidx.room.gradle.plugin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
